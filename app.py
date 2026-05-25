5464b88026af883872a2bc33a2ab712b80cde16b
WillKirk <willkirkpatrick097@gmail.com>
2026-05-25 17:40:24 +0100

    added base template and About page

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def index():
     ]
     images.sort()
     return render_template('index.html', images=images)
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
