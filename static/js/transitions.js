5a8818909971b6ce333766b9dc3239dc8c1ef69c
WillKirk <willkirkpatrick097@gmail.com>
2026-05-25 21:55:48 +0100

    added active nav links and page transitions

---
+++ static/js/transitions.js
@@ -0,0 +1,5 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const main = document.querySelector('main');
+    main === null || main === void 0 ? void 0 : main.classList.add('opacity-100');
+});
---
+++ static/ts/transitions.ts
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const main = document.querySelector('main') as HTMLElement;
+    main?.classList.add('opacity-100');
+});
